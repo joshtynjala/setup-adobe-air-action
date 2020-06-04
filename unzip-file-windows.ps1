3cffa21cd33dc542d4091b27be95883eb759cf33
Josh Tynjala <joshtynjala@bowlerhat.dev>
2020-06-04 10:32:35 -0700

    remove unncessary files

--- unzip-file-windows.ps1
+++
@@ -1,9 +0,0 @@
-param([string]$file,[string]$output)
-
-# usage: powershell ./unzip-file-windows.ps1 -file filename.zip -output C:\some\target\path
-
-$temp = "$PSScriptRoot\unzipped"
-Expand-Archive -LiteralPath "$PSScriptRoot\$file" -DestinationPath $temp
-Set-Location -Path $PSScriptRoot\unzipped\haxe*
-dir
-xcopy * $output /s /y
